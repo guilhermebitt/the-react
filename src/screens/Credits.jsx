5b92641cfd83317be36dc625db748fb4604a6bf5
Guilherme Bitt. <guilherme.assis.bittencourt@gmail.com>
2025-09-14 20:33:51 -0300

    fix(hr): hr

--- src/screens/Credits.jsx
+++ src/screens/Credits.jsx
@@ -71,6 +71,7 @@ function Credits() {
           <p>GitHub: <a href='https://github.com/guilhermebitt' target='_blank'>https://github.com/guilhermebitt</a></p>
           <p>Email: guilherme.assis.bittencourt@gmail.com/</p>
         </div>
+        <hr />
         {/* BETA TESTERS */}
         <h2>Beta Testers</h2>
         {/* Guilherme Bittencourt */}
